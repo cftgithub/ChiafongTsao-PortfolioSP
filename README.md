852c62091085e320b14d69856aa5a4092af75407
cftgithub <chiafongt@gmail.com>
2020-11-05 15:05:56 -0700

    added greeting and current day

--- README.md
+++ README.md
@@ -6,6 +6,11 @@ Responsive single page portfolio.
 ## Status:
 In progress
 
+## Features
+* Displays current day and appropriate greeting for the time of day.
+
+## Technologies
+* JavaScript
 * Bootstrap
 * Sticky navbar
 * Flex-row-reverse
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,31 @@
-// alert("hi!")
+var today = new Date();
+
+function greeting() {
+    var hour = today.getHours();
+    if (hour < 12) {
+        hour = "Good Morning! "
+    }
+    if (hour >= 12 && hour < 18) {
+        hour = "Good Afternoon! "
+    }
+    if (hour >= 18){
+        hour = "Good Evening! "
+    }
+    console.log(hour);
+
+    var day = today.getDay();
+    var dayofweek = [
+        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+    ]
+    var month = today.getMonth();
+    var monthList = [
+        "January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"
+    ]
+    var date = today.getDate() + ", ";
+    var year = today.getFullYear();
+
+    document.getElementById("date").innerHTML =
+        hour + "Today is " + dayofweek[day] + ", " + monthList[month] + " " + date + year;
+}
+greeting();
